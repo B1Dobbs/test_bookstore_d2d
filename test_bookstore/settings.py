65cfc5ae018bc874315abe3552d6e943aed983ce
brennanvandyke <37941104+brennanvandyke@users.noreply.github.com>
2020-01-29 16:50:25 -0600

    Merge branch '47_bookDataStructure' into 51-UploadOnixEndpoint

--- test_bookstore/settings.py
+++ test_bookstore/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'test_bookstore_app'
+    'test_bookstore_app',
     'rest_framework',
 ]
 
--- test_bookstore_app/models.py
+++ test_bookstore_app/models.py
@@ -6,3 +6,22 @@ class File(models.Model):
         return self.file.name
 
     
+# Create your models here.
+
+class AuthorsList(models.Model):
+    name = models.CharField(max_length=250)
+    
+class Book(models.Model):
+    isbn = models.CharField(primary_key=True, max_length=13)
+    title = models.CharField(max_length=250)
+    authors = models.ForeignKey(AuthorsList, on_delete=models.CASCADE)
+    price = models.FloatField()
+    release_date = models.DateTimeField('release date')
+    availability = models.BooleanField()
+    description = models.CharField(max_length=1024)
+    series = models.CharField(max_length=250)
+    vol_num = models.IntegerField()
+
+
+class Library(models.Model):
+    books = models.ForeignKey(Book, on_delete=models.CASCADE)
