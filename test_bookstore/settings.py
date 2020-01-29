0e6cabf383f2a72625923c115199cfa36c29950f
brennanvandyke <37941104+brennanvandyke@users.noreply.github.com>
2020-01-29 16:44:46 -0600

    Changed a couple lines in Settings

--- test_bookstore/settings.py
+++ test_bookstore/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'test_bookstore_app'
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
