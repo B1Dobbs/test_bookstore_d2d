7a1b06aeb8be3403f274d6b7d2b266efbd7f9c76
unknown <catherine.gates@eagles.oc.edu>
2020-01-29 13:02:37 -0800

    Added the base urls, template, and views file

--- test_bookstore_app/views.py
+++ test_bookstore_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def book_detail(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+'''class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'test_bookstore_app/book-detail.html'''
