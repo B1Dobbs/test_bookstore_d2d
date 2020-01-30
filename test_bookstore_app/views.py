67e7f7396e378ae7cccefc3f039e4830d070cfdf
Tmat12 <tyler.mathews12@hotmail.com>
2020-01-30 15:57:50 -0600

    created book list view and a default template

--- test_bookstore_app/views.py
+++ test_bookstore_app/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+from .models import Book
 
 # Create your views here.
+def library(request):
+    template = loader.get_template('test_bookstore_app/library.html')
+    #try:
+    book_list = Book.objects.all()
+    #except Book.DoesNotExist:
+        #raise Http404("No books are in the library")
+    context = {
+        'book_list': book_list,
+    }
+    return HttpResponse(template.render(context, request))
