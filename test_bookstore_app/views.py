434bbc1ad200913b9af9c4ac8fd56aa46b456d4a
Nathan Schrader <nathan@vuduconsulting.com>
2020-01-31 14:29:17 -0600

    add book_detail

--- test_bookstore_app/views.py
+++ test_bookstore_app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-
+from django.views.generic.detail import DetailView
+from django.shortcuts import get_object_or_404
 from .models import Book
 
 # Create your views here.
@@ -15,3 +16,11 @@ def library(request):
         'book_list': book_list,
     }
     return HttpResponse(template.render(context, request))
+
+class BookDetailView(DetailView):
+    model = Book
+    template = loader.get_template('test_bookstore_app/book_detail.html')
+    def get(self, request, *args, **kwargs):
+        book = get_object_or_404(Book, pk=kwargs['pk'])
+        context = {'book': book}
+        return render(request, 'books/book_detail.html', context)
