fb39b990a7008627f7dd369df74ee15ed35650e8
catherinegates97 <catherine.gates@eagles.oc.edu>
2020-02-03 14:08:12 -0600

    added changes from book list view

--- test_bookstore_app/views.py
+++ test_bookstore_app/views.py
@@ -2,39 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
-<<<<<<< HEAD
-def book_list(request):
-    return HttpResponse("Hello, you are on the book list page")
-
-def book_detail(request):
-    title = "Harry Potter and the Sorcerer's Stone"
-    author = "J.K. Rowling"
-    price = "$11.69"
-    release_date = "June 26, 1997"
-    isbn = "9780590353427"
-    availability = "for sale"
-    description = "Harry Potter has never been the star of a Quidditch team, scoring points while riding a broom far above the ground. He knows no spells, has never helped to hatch a dragon, and has never worn a cloak of invisibility."
-    series = "Harry Potter"
-    volume_number = "2"
-    book_id = "9780590353427"
-
-    template = loader.get_template('book_detail.html')
-    context = {
-        'title' : title,
-        'author' : author,
-        'price' : price,
-        'release_date' : release_date,
-        'isbn' : isbn,
-        'availability' : availability,
-        'description' : description,
-        'series' : series,
-        'volume_number' : volume_number,
-        'book_id' : book_id
-    }
-    return HttpResponse(template.render(context, request))
-
-
-=======
 from .models import Book
 
 # Create your views here.
@@ -45,4 +12,3 @@ def library(request):
         'book_list': book_list,
     }
     return HttpResponse(template.render(context, request))
->>>>>>> a925a115da70cc2336f4b92d1ad23146a2a69702
