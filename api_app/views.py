c26e6b2efaa510f2323a0c9e457535830e95671c
brennanvandyke <37941104+brennanvandyke@users.noreply.github.com>
2020-02-04 14:58:30 -0600

    End of day

--- api_app/views.py
+++ api_app/views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
 from .serializers import FileSerializer
-from .models import File
+from .models import OnixFile
 
 from .utils import OnixParser
 from django.http import JsonResponse
