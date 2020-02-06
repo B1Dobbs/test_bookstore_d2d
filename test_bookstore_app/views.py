6e78a3e7dc02933c121f798d41ed194ce7b0ee59
catherinegates97 <catherine.gates@eagles.oc.edu>
2020-02-06 16:11:35 -0600

    Completed Show More functionality

--- test_bookstore_app/views.py
+++ test_bookstore_app/views.py
@@ -12,7 +12,7 @@ def book_detail(request):
     release_date = "June 26, 1997"
     isbn = "9780590353427"
     availability = False
-    description = "Harry Potter has never been the star of a Quidditch team, scoring points while riding a broom far above the ground. He knows no spells, has never helped to hatch a dragon, and has never worn a cloak of invi"
+    description = "Harry Potter has never been the star of a Quidditch team, scoring points while riding a broom far above the ground. He knows no spells, has never helped to hatch a dragon, and has never worn a cloak of invisibility. All he knows is a miserable life with the Dursleys, his horrible aunt and uncle, and their abominable son, Dudley -- a great big swollen spoiled bully. Harry's room is a tiny closet at the foot of the stairs, and he hasn't had a birthday party in eleven years. But all that is about to change when a mysterious letter arrives by owl messenger: a letter with an invitation to an incredible place that Harry -- and anyone who reads about him -- will find unforgettable."
     series = "Harry Potter"
     volume_number = "2"
     book_id = "9780590353427"
