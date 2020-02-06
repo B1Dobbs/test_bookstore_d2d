52f4b3d735d51e259008947ce389ff633a9a5626
B1Dobbs <brookedobbins5@gmail.com>
2020-02-06 14:13:18 -0600

    Saving work due to computer crashing

--- api_app/utils.py
+++ api_app/utils.py
@@ -1,5 +1,6 @@
 from .models import OnixFile
 from lxml import etree
+from test_bookstore_app.models import Book
 
 class OnixParser():
 
@@ -13,15 +14,27 @@ class OnixParser():
         root = etree.fromstring(xml)
         return root
 
-    def getSenderName(root):
+    def get_sender_name(root):
         result = root.xpath("//SenderName")
         for element in result:
             return element.text
 
-    def process_product(product_root):
+    def get_product_isbn_13(product_root):
+        productId = product_root.xpath("ProductIdentifier")
+        for singleID in productId:
+            idType = elem.xpath("ProductIDType")
+            idValue = elem.xpath("IDValue")
+            if(idType == "15"):
+                return idValue
+
+    def process_products(root):
+        result = root.xpath("//Product") 
+        for element in result:
+            book = Book()
+            book.isbn = get_product_isbn_13(element)     
         return True
 
     # TODO
-    def process_onix(file_path):
+    def process_onix(self, file_path):
         root = OnixParser.get_root(file_path)
         return OnixParser.getSenderName(root)
