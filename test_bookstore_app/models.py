b929f0b051553593ec15b19bf9ea8dac20a8cd80
Tmat12 <tyler.mathews12@hotmail.com>
2020-02-10 18:02:56 -0600

    updated database, added languages and updated authors

--- test_bookstore_app/models.py
+++ test_bookstore_app/models.py
@@ -1,23 +1,17 @@
 from django.db import models
 
 # Create your models here.
-
-class AuthorsList(models.Model):
-    name = models.CharField(max_length=250)
-
-    def __str__(self):
-        return self.name
-    
 class Book(models.Model):
     isbn = models.CharField(primary_key=True, max_length=13)
     title = models.CharField(max_length=250)
-    authors = models.ForeignKey(AuthorsList, on_delete=models.CASCADE)
+    authors = models.CharField(max_length=250)
     price = models.FloatField()
     release_date = models.DateField('release date')
     availability = models.BooleanField()
     description = models.CharField(max_length=1024)
     series = models.CharField(max_length=250, blank=True)
     vol_num = models.IntegerField(null=True, blank=True)
+    languages = models.CharField(max_length=20, default='eng')
 
     def __str__(self):
         return self.title + " (" + self.isbn + ")"
