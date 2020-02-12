9a9e30fbe42790ef7d3f62b8457dfe281be7f39a
Brooke Dobbins <brooke.dobbins@eagles.oc.edu>
2020-02-12 16:05:17 -0600

    MC-77: Fixed relative xpath

--- api_app/utils/onix_parser.py
+++ api_app/utils/onix_parser.py
@@ -8,18 +8,26 @@ import datetime
 """Used to include namespace from any Onix file for simple xpath usage.
 Reference: https://stackoverflow.com/questions/5572247/how-to-find-xml-elements-via-xpath-in-python-in-a-namespace-agnostic-way 
 """
+
 def xpath_ns(tree, expr):
+    relative = False
+    if expr[0] == ".":
+        relative = True
+        expr = expr[1:]
+
     "Parse a simple expression and prepend namespace wildcards where unspecified."
     qual = lambda n: n if not n or ':' in n else '*[local-name() = "%s"]' % n
-    expr = '/'.join(qual(n) for n in expr.split('/'))
+    expr = '/'.join(qual(n) for n in expr.split('/')) 
     nsmap = dict((k, v) for k, v in tree.nsmap.items() if k)
+
+    if relative:
+        expr = "." + expr
     return tree.xpath(expr, namespaces=nsmap)
 
 def get_root(xmlFile):
     """
     Get XML Root
     """
-
     with open(xmlFile) as fobj:
         encoding = fobj.readline()
         fobj.seek(0)
@@ -32,9 +40,6 @@ def get_root(xmlFile):
         root = etree.fromstring(xml.encode('utf-16'))
     else:
         root = etree.fromstring(xml)
-
-    ##Example xpath_ns usage
-    ##print(xpath_ns(root, '//Sender'))
     return root
 
 def get_isbn_13(product_root):
@@ -48,8 +53,8 @@ def get_isbn_13(product_root):
 
 def get_collection(product_root):
     collection = {}
-    title = product_root.xpath(".//Collection//TitleText")
-    num = product_root.xpath(".//Collection//PartNumber")
+    title = xpath_ns(product_root, ".//Collection//TitleText")
+    num = xpath_ns(product_root, ".//Collection//PartNumber")
 
     if len(title) != 0:
         collection['title'] = title[0].text
@@ -62,39 +67,39 @@ def get_collection(product_root):
     return collection
 
 def get_title(product_root):
-    result = product_root.xpath(".//TitleDetail")
+    result = xpath_ns(product_root, ".//TitleDetail")
     for titleDetail in result:
-        level = titleDetail.xpath(".//TitleElementLevel")
-        title = titleDetail.xpath(".//TitleText")
+        level = xpath_ns(titleDetail, ".//TitleElementLevel")
+        title = xpath_ns(titleDetail, ".//TitleText")
         if(level[0].text == "01"):
             return title[0].text
     return 0
 
 def get_contributors(product_root):
     authors = []
-    result = product_root.xpath(".//Contributor")
+    result = xpath_ns(product_root, ".//Contributor")
     for contributor in result:
-        role = contributor.xpath(".//ContributorRole")
-        name = contributor.xpath(".//PersonName")
+        role = xpath_ns(contributor,".//ContributorRole")
+        name = xpath_ns(contributor, ".//PersonName")
         if role[0].text == "A01":
             authors.append(name[0].text)
     return authors
 
 def get_language(product_root):
-    language = product_root.xpath(".//LanguageCode")
+    language = xpath_ns(product_root, ".//LanguageCode")
     return language[0].text
 
 def get_detail(product_root):
-    result = product_root.xpath(".//CollateralDetail//TextContent")
+    result = xpath_ns(product_root, ".//CollateralDetail//TextContent")
     for detail in result:
-        text_type = detail.xpath(".//TextType")
-        text = detail.xpath(".//Text")
+        text_type = xpath_ns(detail, ".//TextType")
+        text = xpath_ns(detail, ".//Text")
         if text_type[0].text == "03":
             return text[0].text
     return 0
 
 def get_availability(product_root):
-    result = product_root.xpath(".//PublishingStatus")
+    result = xpath_ns(product_root, ".//PublishingStatus")
    # print(result[0].text)
     if result[0].text == "04":
         return True
@@ -104,10 +109,10 @@ def get_availability(product_root):
     return -1
 
 def get_publish_date(product_root):
-    result = product_root.xpath(".//PublishingDate")
+    result = xpath_ns(product_root, ".//PublishingDate")
     for publishingDate in result:
-        role = publishingDate.xpath("./PublishingDateRole")
-        date = publishingDate.xpath("./Date")
+        role = xpath_ns(publishingDate, "./PublishingDateRole")
+        date = xpath_ns(publishingDate, "./Date")
         if role[0].text == "01":
             date_string = date[0].text
             year = int(date_string[0:4])
@@ -117,10 +122,10 @@ def get_publish_date(product_root):
     return 0
 
 def get_price(product_root):
-    result = product_root.xpath(".//Price")
+    result = xpath_ns(product_root, ".//Price")
     for price in result:
-        price_type = price.xpath("./PriceType")
-        amount = price.xpath("./PriceAmount")
+        price_type = xpath_ns(price, "./PriceType")
+        amount = xpath_ns(price, "./PriceAmount")
         if price_type[0].text == "01":
             return amount[0].text
     return 0
@@ -130,7 +135,7 @@ def process_products(root):
     for product in result:
         #print("\nBook: ")
         book = Book()
-        book.isbn = get_isbn_13(product)  
+        book.isbn = get_isbn_13(product)   
         #print(get_isbn_13(product))  
 
         collection = get_collection(product)
@@ -142,14 +147,16 @@ def process_products(root):
             #print(collection['num'])
 
         book.title = get_title(product)
-        #print(get_title(product))
+        #print(get_title(product, root))
 
         authors = get_contributors(product)
-        authors_str = authors[0]
-        for x in range(1, len(authors)):
-            author_str += ", " + authors[x]
-        book.authors = authors_str
-        #print(get_contributors(product))
+
+        if len(authors) > 0:
+            authors_str = authors[0]
+            for x in range(1, len(authors)):
+                authors_str += ", " + authors[x]
+            book.authors = authors_str
+            #print(get_contributors(product))
 
         book.language = get_language(product)
         #print(get_language(product))
@@ -174,10 +181,7 @@ def process_products(root):
 
 def process_onix(file_path):
     root = get_root(file_path)
-
     process_products(root)
-    #print(OnixFile.load("1"))
-    #print(os.path.exists("../resources/example.xml"))
 
 if __name__ == "__main__":
     process_onix("../resources/large_example.xml")
