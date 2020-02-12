2d2e22aae461b5669bb192cb3bd83a008f887d56
Tmat12 <tyler.mathews12@hotmail.com>
2020-02-12 15:20:50 -0600

    Everything can has a default or can be null/blank

--- test_bookstore_app/models.py
+++ test_bookstore_app/models.py
@@ -3,12 +3,12 @@ from django.db import models
 # Create your models here.
 class Book(models.Model):
     isbn = models.CharField(primary_key=True, max_length=13)
-    title = models.CharField(max_length=250)
-    authors = models.CharField(max_length=250)
-    price = models.FloatField()
-    release_date = models.DateField('release date')
-    availability = models.BooleanField()
-    description = models.CharField(max_length=1024)
+    title = models.CharField(max_length=250, blank=True)
+    authors = models.CharField(max_length=250, blank=True)
+    price = models.FloatField(default=0.00)
+    release_date = models.DateField('release date', null=True)
+    availability = models.BooleanField(default=False)
+    description = models.CharField(max_length=1024, blank=True)
     series = models.CharField(max_length=250, blank=True)
     vol_num = models.IntegerField(null=True, blank=True)
     languages = models.CharField(max_length=20, default='eng')
