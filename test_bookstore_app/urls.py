64952a23da478b80aa6859841c42d051e7af8a37
Nathan Schrader <nathan.schrader@eagles.oc.edu>
2020-02-11 20:17:16 -0600

    update search

--- test_bookstore_app/urls.py
+++ test_bookstore_app/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from .views import SearchResultsView
 
 urlpatterns = [
     path('library/', views.library, name='library'),
     path('<int:pk>/', views.BookDetailView.as_view(), name='detail'),
+        path('search/', SearchResultsView.as_view(), name='search_results'),
     
 ]
--- test_bookstore_app/views.py
+++ test_bookstore_app/views.py
@@ -4,7 +4,8 @@ from django.template import loader
 from django.views.generic.detail import DetailView
 from django.shortcuts import get_object_or_404
 from .models import Book
-
+from django.views.generic import TemplateView, ListView
+from django.db.models import Q
 # Create your views here.
 def library(request):
     template = loader.get_template('library.html')
@@ -24,3 +25,13 @@ class BookDetailView(DetailView):
         book = get_object_or_404(Book, pk=kwargs['pk'])
         context = {'book': book}
         return render(request, 'test_bookstore_app/book_detail.html', context)
+
+class SearchResultsView(ListView):
+    model = Book
+    template_name = 'search_results.html'    
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        object_list = Book.objects.filter(
+            Q(title__icontains=query) | Q(isbn__icontains=query)
+        )
+        return object_list
