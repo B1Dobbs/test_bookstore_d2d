adf28acfeec03ede67767c9f51941cdfb3c08fad
Nathan Schrader <nathan.schrader@eagles.oc.edu>
2020-02-12 14:02:18 -0600

    Add proper url

--- test_bookstore_app/urls.py
+++ test_bookstore_app/urls.py
@@ -3,6 +3,7 @@ from . import views
 from .views import SearchResultsView
 
 urlpatterns = [
+    path('', views.library, name='library'),
     path('library/', views.library, name='library'),
     path('library/<int:pk>/', views.BookDetailView.as_view(), name='detail'),
     path('search/', SearchResultsView.as_view(), name='search_results'),
